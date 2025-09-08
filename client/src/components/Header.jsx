1a0277c2ef3d32de31da1c1f28121c5254b5d123
Arman Ul Haq <armanulhaq@Armans-MacBook-Air.local>
2025-09-08 19:44:32 +0530

    add header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="max-w-5xl mx-auto my-5 flex items-center justify-between px-8 py-4 border border-[#FCFCFC] shadow-[0_0_20px_rgba(127,127,127,0.15)]  bg-white rounded-full">
+            <div className="flex items-center">
+                <img src="/icon.png" alt="Logo" className="h-8 w-8" />
+            </div>
+
+            <ul className="flex items-center gap-8 text-gray-800 font-medium">
+                <li className="cursor-pointer hover:text-purple-600">Home</li>
+                <li className="cursor-pointer hover:text-purple-600">
+                    Find Jobs
+                </li>
+                <li className="cursor-pointer hover:text-purple-600">
+                    Find Talents
+                </li>
+                <li className="cursor-pointer hover:text-purple-600">
+                    About us
+                </li>
+                <li className="cursor-pointer hover:text-purple-600">
+                    Testimonials
+                </li>
+            </ul>
+
+            <button className="bg-gradient-to-b from-[#A128FF] to-[#6100AD] text-white font-medium px-6 py-2 rounded-full shadow-md hover:opacity-90 transition duration-300">
+                Create Jobs
+            </button>
+        </nav>
+    );
+};
+
+export default Navbar;
